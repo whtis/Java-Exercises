c2a4272e826596efdf121fe4afd7101c39c3bb32
whtisw <whtisw@gmail.com>
2016-02-11 16:47:21 +0800

    test

--- chapter13/src/Test.java
+++ chapter13/src/Test.java
@@ -15,7 +15,7 @@ public class Test extends JFrame {
 
     public static void main(String[] args) {
         Test t = new Test();
-        t.pack();
+        t.setSize(300,400);
         t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         t.setVisible(true);
     }
