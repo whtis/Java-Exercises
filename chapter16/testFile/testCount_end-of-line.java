92a636181fbcb2838a005d93312f37ec45bc3905
whtisw <whtisw@gmail.com>
2016-02-20 15:14:54 +0800

    end

---
+++ chapter16/testFile/testCount_end-of-line.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+/**
+ * Created by ht on 2016/2/17.
+ */
+public class Copy {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+
+            System.out.println("Usage: java CopyFile sourceFile targetFile");
+            System.exit(0);
+        }
+
+        File sourceFile = new File("testFile/" + args[0]);
+        if (!sourceFile.exists()) {
+
+            System.out.println("SourceFile " + args[0] + " is not existed");
+            System.exit(0);
+        }
+        File targetFile = new File("testFile/" + args[1]);
+        if (targetFile.exists()) {
+
+            System.out.println("Target file " + args[1] + " already exist");
+            System.exit(0);
+        }
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sourceFile));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(targetFile));
+
+        System.out.println("the file" + args[0] + " has " + bis.available() + " bytes");
+
+        int r;
+        while ((r = bis.read()) != -1) {
+
+            bos.write((byte) r);
+        }
+
+        bis.close();
+        bos.flush();
+        bos.close();
+    }
+
+}
---
+++ chapter16/testFile/testFileViewerCopy.w
@@ -0,0 +1,10 @@
+deSXMOjYGU
+P[vPATzhVO
+BUewZKEqVb
+`FIaoVgQ[]
+XTmum]AoQU
+zH^advgBre
+gTwsMvCinT
+iZrttlQPdy
+SwGkW\][l]
+iySlRISv_p
